aaa39643976c62ecfe5ef4c5f0dddf58895738ef
Akash <123761006+Akash19304@users.noreply.github.com>
2024-08-30 21:24:57 +0530

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="ecommerce",
+    version="0.1",
+    packages=find_packages(),
+    install_requires=[unittest],  # Add any dependencies here
+)
